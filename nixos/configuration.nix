dae1c91d38ae1695b8bce0e0fd9146efdbe1b3e4
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2023-09-19 00:55:36 +0200

    Removed the binary chache example

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,13 +97,11 @@
     # Enable the binary cache
     settings = {
       substituters = [
-        "http://binarycache.example.com"
-	"https://nix-community.cachix.org"
-	"https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+	      "https://cache.nixos.org"
       ];
       trusted-public-keys = [
-        "binarycache.example.com-1:dsafdafDFW123fdasfa123124FADSAD"
-	"nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+	      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
