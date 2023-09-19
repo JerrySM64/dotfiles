b700adfa73dffde06f958e4e9348461b30443a60
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2023-09-19 02:04:45 +0200

    Fixed the URL for unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # change to github:nixos/nixpkgs/nixos-23.05 for unstable
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
 
     home-manager = {
