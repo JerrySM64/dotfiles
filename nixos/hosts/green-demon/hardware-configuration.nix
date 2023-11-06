8184a44730bb00bfbd0dec95628eeaa51f8644d0
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2023-11-06 22:49:21 +0100

    Fix mountpoints and cleanup

--- nixos/hosts/green-demon/hardware-configuration.nix
+++ nixos/hosts/green-demon/hardware-configuration.nix
@@ -4,24 +4,36 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f8a3c0b6-c20b-4e45-af30-d7ea47bcddf4";
-      fsType = "btrfs";
+  boot = {
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
+      kernelModules = [ ];
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0EE7-443E";
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
+
+  fileSystems = {
+    "/" = { 
+      device = "/dev/disk/by-label/NixOS";
+      fsType = "xfs";
+    };
+
+    "/boot" = { 
+      device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
     };
+  
+    "/home" = {
+      device = "/dev/disk/by-label/Home";
+      fsType = "xfs";
+    };
+  };
 
   swapDevices = [ ];
 
@@ -29,9 +41,14 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+  networking = {
+    useDHCP = lib.mkDefault true;
+  
+  #  interfaces = {
+    #  eno1.useDHCP = lib.mkDefault true;
+    #  wlp5s0.useDHCP = lib.mkDefault true;
+    #};
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
