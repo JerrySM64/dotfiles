949b776537219ee00d7be2b93d6013bde4a8bbbe
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2023-12-31 13:06:11 +0100

    Symlinks are now reproducible

--- nixos/home.nix
+++ nixos/home.nix
@@ -6,16 +6,16 @@
     file = {
       # Starship prompt
       ".config/starship.toml" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dotfiles/dots/.config/starship.toml";
+        source = ../dots/.config/starship.toml;
       };
       # Neofetch
       ".config/neofetch" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dotfiles/dots/.config/neofetch";
+        source = ../dots/.config/neofetch;
         recursive = true;
       };
       # Z-Shell
       ".zshrc" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dots/.zshrc";
+        source = ../dots/.zshrc;
       };
     };
   };
--- nixos/wm-config/hyprconf.nix
+++ nixos/wm-config/hyprconf.nix
@@ -6,22 +6,22 @@
     file = {
       # Hyprland
       ".config/hypr" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dotfiles/dots/.config/hypr";
+        source = ../../dots/.config/hypr;
         recursive = true;
       };
       # Waybar
       ".config/waybar" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dotfiles/dots/.config/waybar";
+        source = ../../dots/.config/waybar;
         recursive = true;
       };
       # Kitty
       ".config/kitty" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dotfiles/dots/.config/kitty";
+        source = ../../dots/.config/kitty;
         recursive = true;
       };
       # Rofi
       ".config/rofi" = {
-        source = config.lib.file.mkOutOfStoreSymlink "/home/Jerry/Development/dotfiles/dots/.config/rofi";
+        source = ../../dots/.config/rofi;
         recursive = true;
       };
     };
