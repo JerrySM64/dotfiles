84f80d9b2a24dbde09299eef9460158e2bf86f90
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2024-01-19 11:59:48 +0100

    Add v4l2loopback module for virtual camera support

--- nixos/obs.nix
+++ nixos/obs.nix
@@ -2,6 +2,11 @@
 
 {
   config = {
+    boot = {
+      kernelModules = [ "v4l2loopback" ];
+      extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    };
+
     environment.systemPackages = with pkgs; [
       obs-studio
       obs-studio-plugins.input-overlay
