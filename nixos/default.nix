3d97b9e355a0e612928c8995728f9a973074cfed
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2024-02-05 03:06:35 +0100

    Some sorting

--- nixos/default.nix
+++ nixos/default.nix
@@ -52,15 +52,6 @@
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = [
-    "/etc/nix/path"
-  ];
   environment = {
     etc =
       lib.mapAttrs'
@@ -86,6 +77,16 @@
       automatic = true;
       options = "--delete-older-than-3d";
     };
+
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = [
+      "/etc/nix/path"
+    ];
   };
 
   # FIXME: Add the rest of your current configuration
