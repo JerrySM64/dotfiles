03d8937ac2b811ef4252649b43f6bc1dcb34ce24
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2024-03-09 14:00:54 +0100

    Add extraPackages.nix for easier addition of packages

---
+++ extraPackages.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }: {
+
+  # Add your extra packages here. They can be space separated or one package per line.
+  # If unsure, you can open up the package search by pressing SUPER + Shift + W and 
+  # choosing "Unstable Packages" to open up the package search.
+  environment.systemPackages = with pkgs; [
+  #  Example:
+  #  wget
+  ];
+}
