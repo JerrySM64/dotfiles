ec77266917b593f34759708ecc5c896d8bab0aa6
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2025-05-08 18:54:40 +0200

    4th try

--- flake.nix
+++ flake.nix
@@ -41,7 +41,6 @@
     inherit (self) outputs;
     # Supported systems for your flake packages, shell, etc.
     systems = [
-      "aarch64-linux"
       "x86_64-linux"
     ];
     # This is a function that generates an attribute by calling a function you
--- home-manager/default.nix
+++ home-manager/default.nix
@@ -75,5 +75,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "22.05";
+  #home.stateVersion = "22.05";
 }
