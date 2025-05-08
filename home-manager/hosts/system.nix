e2150941f23a7c648ce743106688063e7ecf77a3
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2025-05-08 19:29:05 +0200

    Get rid of AnyDesk

--- home-manager/hosts/system.nix
+++ home-manager/hosts/system.nix
@@ -8,20 +8,7 @@
   # System packages
   environment = {
     systemPackages = with pkgs; [
-      anydesk
-      ardour
-      blender
-      brasero
       distrobox
-      gimp
-      inkscape
-      krita
-      libratbag
-      mpv
-      qjackctl
-      spotify
-      virt-manager
-      virt-viewer
     ];
   };
 }
